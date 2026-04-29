60caef63205e2ff9452b8551688b6474223e04c9
Ubuntu <ubuntu@ip-172-31-27-69.ap-south-1.compute.internal>
2026-04-29 13:26:43 +0000

    fix redis config

--- config/redis.js
+++ config/redis.js
@@ -1,9 +1,15 @@
-// config/redis.js
+require("dotenv").config();
 const Redis = require("ioredis");
 
 const redis = new Redis({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
+
+  // 🔥 TLS FIX
+  tls: {},
 });
+
+redis.on("connect", () => console.log("✅ Redis connected"));
+redis.on("error", (err) => console.error("❌ Redis error:", err));
 
 module.exports = redis;
